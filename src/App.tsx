c027dbd00961298e46a129c08eb932a858e63d4a
danielmargolin <margolin23@gmail.com>
2024-07-26 16:15:50 +0300

    styled the graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,63 @@
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import "./App.css";
 import { LineChart } from "@mui/x-charts/LineChart";
+import data from "./data.json";
+
+const steps = data.map(({ step }) => step);
+const waitingTimes = data.map(
+  ({ system_mean_waiting_time }) =>
+    system_mean_waiting_time
+);
 
 function App() {
   return (
     <>
+      <FormControl>
+        <InputLabel id="demo-simple-select-label">Intersection</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={1}
+          label="Intersection"
+          onChange={() => {}}
+        >
+          <MenuItem value={1}>1</MenuItem>
+          <MenuItem value={2}>2</MenuItem>
+          <MenuItem value={3}>3</MenuItem>
+          <MenuItem value={4}>4</MenuItem>
+          <MenuItem value={5}>5</MenuItem>
+          <MenuItem value={6}>6</MenuItem>
+          <MenuItem value={7}>7</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl>
+        <InputLabel id="demo-simple-select-label">Experiment date</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={1}
+          label="Experiment date"
+          onChange={() => {}}
+        >
+          <MenuItem value={1}>1</MenuItem>
+          <MenuItem value={2}>2</MenuItem>
+          <MenuItem value={3}>3</MenuItem>
+          <MenuItem value={4}>4</MenuItem>
+          <MenuItem value={5}>5</MenuItem>
+          <MenuItem value={6}>6</MenuItem>
+          <MenuItem value={7}>7</MenuItem>
+        </Select>
+      </FormControl>
       <LineChart
-        xAxis={[{ data: [1, 2, 3, 5, 8, 10], id: "x-axis-id", scaleType: 'band', }]}
+        xAxis={[{ data: steps, id: "x-axis-id", scaleType: "band" }]}
         series={[
           {
-            data: [2, 5.5, 2, 8.5, 1.5, 5],
+            data: waitingTimes,
+            showMark: false,
           },
         ]}
-        width={500}
-        height={300}
+        width={800}
+        height={500}
       />
     </>
   );
